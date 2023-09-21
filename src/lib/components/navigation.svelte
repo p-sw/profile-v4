8e240809e3659f2512c5422732e5f0170f0a3832
p-sw <shinwoo.park@psw.kr>
2023-09-21 12:42:24 +0000

    feat: add navigation controls in pc

--- src/lib/components/navigation.svelte
+++ src/lib/components/navigation.svelte
@@ -2,9 +2,8 @@
 	import { slide } from 'svelte/transition';
 	import { linear } from 'svelte/easing';
 	import { page } from '$app/stores';
-	import Icon from 'svelte-icons-pack';
-	import AiFillGithub from 'svelte-icons-pack/ai/AiFillGithub';
 	import { onMount } from 'svelte';
+	import NavControls from './navcontrols.svelte';
 
 	/** @type boolean */
 	let navOpened = false;
@@ -38,6 +37,9 @@
 		{#each Object.entries(routes) as [route, routeName]}
 			<a href={route} class={$page.url.pathname === route ? 'active' : ''}>{routeName}</a>
 		{/each}
+		<aside>
+			<NavControls {theme} />
+		</aside>
 	</div>
 	{#if navOpened}
 		<span
@@ -87,40 +89,7 @@
 {#if navOpened}
 	<div class="router" transition:slide={{ duration: 200, easing: linear, axis: 'y' }}>
 		<div>
-			<span
-				on:click={() => {
-					theme = theme === 'dark' ? 'light' : 'dark';
-					document.cookie = `theme=${theme};`;
-					document.documentElement.setAttribute('data-theme', theme);
-				}}
-				on:keypress={() => {}}
-				role="button"
-				tabindex={0}
-			>
-				{#if theme === 'light'}
-					<svg xmlns="http://www.w3.org/2000/svg" height="20" viewBox="0 -960 960 960" width="20"
-						><path
-							d="M480-360q50 0 85-35t35-85q0-50-35-85t-85-35q-50 0-85 35t-35 85q0 50 35 85t85 35Zm0 80q-83 0-141.5-58.5T280-480q0-83 58.5-141.5T480-680q83 0 141.5 58.5T680-480q0 83-58.5 141.5T480-280ZM80-440q-17 0-28.5-11.5T40-480q0-17 11.5-28.5T80-520h80q17 0 28.5 11.5T200-480q0 17-11.5 28.5T160-440H80Zm720 0q-17 0-28.5-11.5T760-480q0-17 11.5-28.5T800-520h80q17 0 28.5 11.5T920-480q0 17-11.5 28.5T880-440h-80ZM480-760q-17 0-28.5-11.5T440-800v-80q0-17 11.5-28.5T480-920q17 0 28.5 11.5T520-880v80q0 17-11.5 28.5T480-760Zm0 720q-17 0-28.5-11.5T440-80v-80q0-17 11.5-28.5T480-200q17 0 28.5 11.5T520-160v80q0 17-11.5 28.5T480-40ZM226-678l-43-42q-12-11-11.5-28t11.5-29q12-12 29-12t28 12l42 43q11 12 11 28t-11 28q-11 12-27.5 11.5T226-678Zm494 495-42-43q-11-12-11-28.5t11-27.5q11-12 27.5-11.5T734-282l43 42q12 11 11.5 28T777-183q-12 12-29 12t-28-12Zm-42-495q-12-11-11.5-27.5T678-734l42-43q11-12 28-11.5t29 11.5q12 12 12 29t-12 28l-43 42q-12 11-28 11t-28-11ZM183-183q-12-12-12-29t12-28l43-42q12-11 28.5-11t27.5 11q12 11 11.5 27.5T282-226l-42 43q-11 12-28 11.5T183-183Zm297-297Z"
-						/></svg
-					>
-				{:else if theme === 'dark'}
-					<svg xmlns="http://www.w3.org/2000/svg" height="20" viewBox="0 -960 960 960" width="20"
-						><path
-							d="M480-120q-150 0-255-105T120-480q0-150 105-255t255-105q14 0 27.5 1t26.5 3q-41 29-65.5 75.5T444-660q0 90 63 153t153 63q55 0 101-24.5t75-65.5q2 13 3 26.5t1 27.5q0 150-105 255T480-120Zm0-80q88 0 158-48.5T740-375q-20 5-40 8t-40 3q-123 0-209.5-86.5T364-660q0-20 3-40t8-40q-78 32-126.5 102T200-480q0 116 82 198t198 82Zm-10-270Z"
-						/></svg
-					>
-				{/if}
-			</span>
-			<span
-				on:click={() => {
-					window.open('https://github.com/p-sw/profile-v4', '_blank');
-				}}
-				on:keypress={() => {}}
-				role="button"
-				tabindex={0}
-			>
-				<Icon src={AiFillGithub} color="var(--base-100-content)" />
-			</span>
+			<NavControls {theme} />
 		</div>
 		{#each Object.entries(routes) as [route, routeName]}
 			<a href={route} class={$page.url.pathname === route ? 'active' : ''}>{routeName}</a>
@@ -158,7 +127,7 @@
 			letter-spacing: -4.5px;
 		}
 
-		& span {
+		& > span {
 			width: 3.5rem;
 			height: 3.5rem;
 
@@ -203,6 +172,16 @@
 					text-decoration-color: var(--primary);
 				}
 			}
+
+			& aside {
+				padding-left: 20px;
+
+				display: flex;
+				flex-direction: row;
+				justify-content: center;
+				align-items: center;
+				gap: 10px;
+			}
 		}
 	}
 	div.router {
@@ -228,6 +207,10 @@
 
 		padding-bottom: 20px;
 
+		@media screen and (min-width: 1000px) {
+			display: none;
+		}
+
 		& div,
 		a {
 			padding: 1rem 0;
@@ -243,18 +226,6 @@
 			align-items: center;
 			padding-right: 4rem;
 			gap: 2rem;
-
-			& span {
-				width: 20px;
-				height: 20px;
-
-				& svg {
-					width: 20px;
-					height: 20px;
-
-					fill: var(--base-100-content);
-				}
-			}
 		}
 		& a {
 			text-decoration: none;
